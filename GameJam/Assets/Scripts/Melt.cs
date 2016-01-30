d9404fca4550e71db59c0001d7d017b580b3091b
Sean <sean@sean>
2016-01-30 16:23:40 -0500

    Candle's Melt

---
+++ GameJam/Assets/Scripts/Melt.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Melt : MonoBehaviour {
+
+	public float speed;
+
+
+	
+	// Update is called once per frame
+	void Update (){
+
+
+
+
+
+		Vector3 vt3 = new Vector3 (transform.localScale.x,(transform.localScale.y- (Time.deltaTime * speed)), transform.localScale.z);
+
+		transform.localScale = vt3;
+
+
+
+	
+	}
+}
