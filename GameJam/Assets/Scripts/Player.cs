c539566d67eeeedcc5b7b431828108ede30f066f
mikestoleyobike <silenthps@aim.com>
2016-01-29 23:57:22 -0500

    IT MOVES YO

---
+++ GameJam/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 10.0f;
+    public float gravity = 20.0f;
+
+    private float horDir;
+    private float vertDir;
+    private Vector3 moveDirection;
+    private CharacterController controller;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        horDir = Input.GetAxis("Horizontal");
+        vertDir = Input.GetAxis("Vertical");
+
+        Debug.Log("Hor: " + horDir);
+        Debug.Log("Ver: " + vertDir);
+        moveDirection = new Vector3(horDir, 0, vertDir);
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= speed;
+
+        // moveDirection.y -= gravity * Time.deltaTime;
+         controller.Move(moveDirection * Time.deltaTime);
+
+	
+	}
+}
---
+++ GameJam/Assets/Scripts/collectible.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collectible : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        bool b = Input.GetButton("Horizontal");
+        float f = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Debug.Log(b.ToString());
+        Debug.Log(f.ToString());
+
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
