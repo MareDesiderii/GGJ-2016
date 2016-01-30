e1365a28aa6f80c1edd1d767339b2bf68174f5b0
mikestoleyobike <silenthps@aim.com>
2016-01-30 09:19:30 -0500

    meow

--- GameJam/Assets/Scripts/Player.cs
+++ GameJam/Assets/Scripts/Player.cs
@@ -7,6 +7,9 @@ public class Player : MonoBehaviour {
     public float speed = 10.0f;
     public float gravity = 20.0f;
 
+    public GameObject otherLight;
+    public float maxDist = 1.0f;
+
     private float horDir;
     private float vertDir;
     private Vector3 moveDirection;
@@ -44,6 +47,21 @@ public class Player : MonoBehaviour {
         // moveDirection.y -= gravity * Time.deltaTime;
          controller.Move(moveDirection * Time.deltaTime);
 
+         if (!CompareDistanceToLight())
+             Destroy(gameObject);
 	
 	}
+
+    bool CompareDistanceToLight()
+    {
+        float dx = transform.position.x - otherLight.transform.position.x;
+        float dy = transform.position.z - otherLight.transform.position.z;
+
+        float distance = Mathf.Sqrt(dx * dx + dy * dy);
+
+        if (distance < maxDist)
+            return true;
+        else
+            return false;
+    }
 }
---
+++ GameJam/Assets/Scripts/begin.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class begin : MonoBehaviour {
+
+    public GameObject redCandle;
+    public GameObject blueCandle;
+
+    private Quaternion rot;
+
+	// Use this for initialization
+	void Start () {
+        rot = new Quaternion(0, 0, 0, 0);
+        Star();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public void Star()
+    {
+        Instantiate(redCandle, new Vector3(0.0f, 1.0f, 1.0f), rot);
+        Instantiate(blueCandle, new Vector3(-.5f, 1.0f, 0.0f), rot);
+        Instantiate(redCandle, new Vector3(-1.0f, 1.0f, 0.0f), rot);
+        Instantiate(redCandle, new Vector3(0.5f, 1.0f, 0.0f), rot);
+        Instantiate(blueCandle, new Vector3(1.0f, 1.0f, 0.0f), rot);
+        Instantiate(blueCandle, new Vector3(-.5f, 1.0f, -.5f), rot);
+        Instantiate(redCandle, new Vector3(0.5f, 1.0f, -0.5f), rot);
+        Instantiate(blueCandle, new Vector3(-0.5f, 1.0f, -1.0f), rot);
+        Instantiate(redCandle, new Vector3(0.5f, 1.0f, -1.0f), rot);
+
+    }
+}
