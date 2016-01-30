512a39d6c5184f4dfcbd0eae08a44dfda20cffba
LexAckson <LexAckson@gmail.com>
2016-01-30 17:10:23 -0500

    Keep flames in light

--- GameJam/Assets/Scripts/Player.cs
+++ GameJam/Assets/Scripts/Player.cs
@@ -8,7 +8,7 @@ public class Player : MonoBehaviour {
     public float gravity = 20.0f;
 
     public GameObject otherLight;
-    public float maxDist = 1.0f;
+    public float maxDist = 0.3f;
 
     private float horDir;
     private float vertDir;
@@ -47,30 +47,33 @@ public class Player : MonoBehaviour {
 
         moveDirection = new Vector3(horDir, 0, vertDir);
         moveDirection = transform.TransformDirection(moveDirection);
-        if (!CompareDistanceToLight())
+        moveDirection *= speed;
+
+        // clamp our movment to just inside the light
+        Vector3 newPos = transform.position + moveDirection;
+        Vector3 oldOffset = transform.position - otherLight.transform.position;
+        Vector3 newOffset = newPos - otherLight.transform.position;
+        Vector3 AllowedMove = Vector3.ClampMagnitude(newOffset, maxDist) - oldOffset;
+        //pull on the other player if we would be moving outside the light
+        if (IsOutsideLight(moveDirection)) 
         {
-            moveDirection *= speed / 5.0f;
-            otherLight.GetComponentInParent<Player>().controller.Move(-moveDirection * Time.deltaTime * .75f);
+            otherLight.GetComponentInParent<Player>().controller.Move(-moveDirection * Time.deltaTime * 0.5f);
         }
-        else
-            moveDirection *= speed;
 
-        // moveDirection.y -= gravity * Time.deltaTime;
-         controller.Move(moveDirection * Time.deltaTime);
-	
-	}
+        controller.Move(AllowedMove * Time.deltaTime);
+    }
 
-    bool CompareDistanceToLight()
+    bool IsOutsideLight(Vector3 move)
     {
-        float dx = transform.position.x - otherLight.transform.position.x;
-        float dy = transform.position.z - otherLight.transform.position.z;
+        float dx = transform.position.x + move.x - otherLight.transform.position.x;
+        float dy = transform.position.z + move.z - otherLight.transform.position.z;
 
         float distance = Mathf.Sqrt(dx * dx + dy * dy);
 
         if (distance < maxDist)
-            return true;
-        else
             return false;
+        else
+            return true;
     }
 
     void OnTriggerExit(Collider col)
