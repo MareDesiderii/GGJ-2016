f39cff8dffb948764a88950d1883b7e405f97389
mikestoleyobike <silenthps@aim.com>
2016-01-30 14:48:57 -0500

    aslj;
    Delete executables

---
+++ GameJam/Assets/Scripts/startgame.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class startgame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void LoadLevel()
+    {
+        Application.LoadLevel(1);
+    }
+}
