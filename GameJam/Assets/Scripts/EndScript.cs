0de622aae20cdc09b7129b7e687ad031ec2083cd
mikestoleyobike <silenthps@aim.com>
2016-01-30 20:29:09 -0500

    ui ui ui ui ui

---
+++ GameJam/Assets/Scripts/EndScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EndScript : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+        Score sco = FindObjectOfType<Score>();
+        selection sel = FindObjectOfType<selection>();
+
+        int div = 0;
+        if (sel.symbol == 0)
+            div = 5;
+        if (sel.symbol == 1)
+            div = 20;
+        if (sel.symbol == 2)
+            div = 23;
+
+        int score = sco.m_Score;
+        GetComponent<Text>().text = "Score: " + score.ToString() + "\\" + div.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
