4dbb7e11a76e178073b7165adfbd11ea8eb3fdfd
Sean <sean@sean>
2016-01-31 03:33:11 -0500

    Debug messages removed

--- GameJam/Assets/Scripts/Melt.cs
+++ GameJam/Assets/Scripts/Melt.cs
@@ -10,21 +10,16 @@ public class Melt : MonoBehaviour {
 	// Update is called once per frame
 	void Update (){
 
-		//If the Candle's cubes mesh renderer is enabled, melt the candle
-	/*	GameObject cube = transform.GetChild(0).FindChild("Cube").gameObject;
-		if (cube.GetComponent<MeshRenderer> ().enabled == true) {*/
-		Debug.Log ("Child "+transform.GetChild (0).gameObject);
-		Debug.Log ("Grand Child "+transform.GetChild (0).GetChild(0).gameObject.name);
+	
 
 		if (transform.GetChild(0).GetChild(0).gameObject.active){
-			Debug.Log ("MELTING!!!");
+			
 			Vector3 vt3 = new Vector3 (transform.localScale.x, transform.localScale.y,transform.localScale.z - (Time.deltaTime * speed));
 
 			transform.localScale = vt3;
 
-			if (vt3.z <= 0) {
-				Debug.Log ("Time to Destroy");
-				Debug.Log ("THIS " + this);
+			if (vt3.y <= 0) {
+				
 				Destroy (this.gameObject);
 				AudioSource candle_out = GameObject.Find( "SoundCandleOut" ).GetComponent<AudioSource>();
 				candle_out.Play();
