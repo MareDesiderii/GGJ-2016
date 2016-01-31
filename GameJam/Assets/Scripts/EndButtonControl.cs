ad3091563dd5d2461d2b064968246098109e72db
LexAckson <LexAckson@gmail.com>
2016-01-31 14:56:37 -0500

    menu stuff

--- GameJam/Assets/Scripts/EndButtonControl.cs
+++ GameJam/Assets/Scripts/EndButtonControl.cs
@@ -6,50 +6,76 @@ using UnityEngine.UI;
 
 public class EndButtonControl : MonoBehaviour
 {
-
     public Button[] buttons;
-    public int activeButton = 0;
+    public int selectedButton = 0;
 
     private ColorBlock cb;
     private float t = 0;
     private Color purple;
     private bool flicker = false;
+
+    private float elapsedTime = 0.0f;
+    private float preventSelectUntil = 1.0f;
+    bool slowInput = false;
+
     // Use this for initialization
     void Start()
     {
-        cb = buttons[activeButton].colors;
+        cb = buttons[selectedButton].colors;
         purple = new Color(0.0f, 1.0f, 0.0f);
+
+        elapsedTime += Time.deltaTime;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        elapsedTime += Time.deltaTime;
+
         float imp = Input.GetAxis("Horizontal");
-        if (imp > 0.4f)
+
+        if (imp < -0.4f || imp > 0.4f)
         {
-            cb.normalColor = Color.white;
-            buttons[activeButton].colors = cb;
-            activeButton++;
-            activeButton = Mathf.Clamp(activeButton, 0, 2);
-            cb = buttons[activeButton].colors;
-            flicker = false;
+
+            if (!slowInput || elapsedTime > preventSelectUntil)
+            {
+                Debug.Log("not preventing select ");
+
+                cb.normalColor = Color.white;
+                buttons[selectedButton].colors = cb;
+
+                if (imp > 0.4f)
+                {
+                    selectedButton++;
+                }
+                else
+                {
+                    selectedButton--;
+                }
+
+                selectedButton = Mathf.Clamp(selectedButton, 0, 2);
+                cb = buttons[selectedButton].colors;
+                flicker = false;
+
+                preventSelectUntil = elapsedTime + 0.4f;
+                slowInput = true;
+            }
+
+
         }
-        if (imp < -0.4f)
+        else
         {
-            cb.normalColor = Color.white;
-            buttons[activeButton].colors = cb;
-            activeButton--;
-            activeButton = Mathf.Clamp(activeButton, 0, 2);
-            cb = buttons[activeButton].colors;
-            flicker = false;
+            slowInput = false;
         }
+
         if (Input.GetButtonDown("Fire1"))
         {
-            if (activeButton == 0)
+            if (selectedButton == 0)
                 GetComponent<endgame>().LoadPeace();
-            if (activeButton == 1)
+            if (selectedButton == 1)
                 GetComponent<endgame>().LoadLove();
-            if (activeButton == 2)
+            if (selectedButton == 2)
                 GetComponent<endgame>().LoadHappy();
 
         }
@@ -63,6 +89,6 @@ public class EndButtonControl : MonoBehaviour
         else
             cb.normalColor -= purple * Time.deltaTime;
 
-        buttons[activeButton].colors = cb;
+        buttons[selectedButton].colors = cb;
     }
 }
